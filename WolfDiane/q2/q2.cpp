5d254cefd6590bc2590318cd1026fb4ce681bf67
youeff <dawolf@sfu.ca>
2017-10-04 04:23:30 +0000

    added new stuff - q2 and lab4

--- WolfDiane/q2/q2.cpp
+++ WolfDiane/q2/q2.cpp
@@ -4,7 +4,8 @@
  * @Date   9/28/2017
  * @version 1.0
  * @section DESCRIPTION
- * This program ...
+ * This program demonstrates a class that acts as a substitute for vectors.
+ * It is defined for doubles.
  */
 // I declare that this assignment is my own work and that I have correctly
 // acknowledged the work of others.  I acknowledged that I have read and
@@ -15,11 +16,58 @@
 //
 // ____ tcl18 | 301320975, dawolf | 301305135 ______
 
+#include "VectorDouble.h" // class header file
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    int customSize;
+    customSize = 0;
+    double input;
+    input = 0.0;
+    int accessPos;
+    accessPos = 0;
+    double value;
+    value = 0.0;
+    
+    cout << "creating object..." << endl;
+/*    
+    VectorDouble test1;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "new entry: ";
+        cin >> input;
+        test1.push_back(input);
+        input = 0.0;
+    }
+    
+    cout << "vector size is currently " << test1.size() << endl;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "position to access: ";
+        cin >> accessPos;
+        value = test1.value_at(accessPos);
+        cout << "value at " << accessPos << " is " << value << endl;
+        value = 0.0;
+        accessPos = 0;
+    }
+*/
+    cout << "enter size of array >= 3: ";
+    cin >> customSize;
+    VectorDouble test2(customSize);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "new entry: ";
+        cin >> input;
+        test2.push_back(input);
+        input = 0.0;
+    }
+    for (int i = 0; i < (2 * customSize); i++)
+    {
+            cout << test2.value_at(i) << endl;
+    }
     
     return 0;
 }
